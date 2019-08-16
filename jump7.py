8f83aee67fa83ef3ade67cb7e92edf1cb3128dc9
yiliunian <yiliunian@163.com>
2019-08-16 15:50:53 +0800

    game one

---
+++ jump7.py
@@ -0,0 +1,11 @@
+a=0
+while a<100:
+    a=a+1
+    if a%7==0:  #7???
+        continue
+    elif a%10==7:  #???7?
+        continue
+    elif a//10==7: #???7?  ??????7?
+        continue
+    else:
+        print(a)
